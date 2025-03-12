8f762050bc247db32fa876e89efdfba2d2aa9aee
Rahelaraya <rahelaraya26@gmail.com>
2025-03-12 10:42:11 +0100

    Add project files.

---
+++ OnsdagKodtest/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OnsdagKodtest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(MestFrekventSiffra(new int[] { 1, 3, 2, 3, 4, 1, 3, 2, 2, 2, 5 })); 
+            Console.WriteLine(MestFrekventSiffra(new int[] { 7, 7, 5, 5, 1, 1, 1, 2, 2, 2 })); 
+        
+        }
+    }
+ 
+}
